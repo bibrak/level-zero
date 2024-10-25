297ed53cd861d31dabcc3d3ac703be853729d8a3
Chandio, Bibrak Qamar <bibrak.qamar.chandio@intel.com>
2024-10-25 10:51:48 -0700

    feature: events deadlock detection in validation layer
    Related-To: NEO-12810
    
    Signed-off-by: Chandio, Bibrak Qamar <bibrak.qamar.chandio@intel.com>

--- source/layers/validation/checkers/parameter_validation/ze_parameter_validation.cpp
+++ source/layers/validation/checkers/parameter_validation/ze_parameter_validation.cpp
@@ -12,8 +12,6 @@
 #include "ze_validation_layer.h"
 #include "param_validation.h"
 
-#include <iostream>
-
 namespace validation_layer
 {
     class parameterValidationChecker parameterChecker;
@@ -45,7 +43,6 @@ namespace validation_layer
                                                         ///< must be 0 (default) or a combination of ::ze_init_flag_t.
         )
     {
-        std::cout << "ZEParameterValidation: zeInitPrologue" << std::endl;
         if( 0x3 < flags )
             return ZE_RESULT_ERROR_INVALID_ENUMERATION;
 
